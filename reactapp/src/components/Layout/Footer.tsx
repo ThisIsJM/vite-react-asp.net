81ee3a4aca685762ed1a2a7d3ff1995b315b9aaf
Joshua Marquez <Joshua.marquez0718@gmail.com>
2023-10-29 23:10:20 +0800

    Updated layout boilerplate design

--- reactapp/src/components/Layout/Footer.tsx
+++ reactapp/src/components/Layout/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="footer p-5 pb-2 shadow-inner absolute bottom-0">
     <nav>
-      <header className="footer-title text-gray-900">
+      <header className="footer-title">
           <a href="https://github.com/ThisIsJM"> Github @ThisIsJm </a>
       </header>
     </nav> 
--- reactapp/src/components/Layout/Header.tsx
+++ reactapp/src/components/Layout/Header.tsx
@@ -3,10 +3,7 @@ import React from 'react';
 export default function Header() {
   return (
     <div className="navbar bg-base-100 shadow-lg">
-        <a className=" normal-case text-xl cursor-pointer">
-            <img className="ml-3 mr-5" src="./react-logo.png" width="40" height="40"></img>
-            React - Asp.Net
-        </a>
+        <a className=" normal-case text-xl cursor-pointer"> React - Asp.Net </a>
     </div>
   )
 }
